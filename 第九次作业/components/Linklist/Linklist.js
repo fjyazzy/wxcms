2fa986f277b0fb4f98fe307ca3d992707d63fea7
llz6 <48794024+llz6@users.noreply.github.com>
2019-05-20 13:17:15 +0800

    Add files via upload

---
+++ 第九次作业/components/Linklist/Linklist.js
@@ -0,0 +1,43 @@
+// Linklist/Linklist.js
+Component({
+  properties: {
+    usite: {
+      type: String,
+      value: 'http://',
+    },
+  },
+
+  data: {
+    ItemList: [
+      { "pagename": "Wxml学习的基本语法", "pagetext": "/pages/Tools/tools" },
+      { "pagename": "利息计算器", "pagetext": "../lxjsq/lxjsq" },
+      { "pagename": "留言板", "pagetext": "../lyb/lyb" },
+      { "pagename": "计算器", "pagetext": "../lxjsq/lxjsq" },
+      { "pagename": "地图", "pagetext": "../map/map" }
+    ],
+  },
+  attached() {
+  },
+  methods: {
+    //显示页面代码
+    showpage: function () {
+      var that = this;
+      wx.request({
+        url: that.data.usite + '/cmsv1/apis/getHomePagelist.ashx?mlh=07',
+        header: { 'content-type': 'applciation/json;charset=UTF-8' },
+        method: 'GET',
+        success: function (res) {
+          var str2 = res.data.substr(1, res.data.length - 2)
+          console.log(str2);
+          that.setData({
+            ItemList: JSON.parse(str2),
+          })
+        },
+        fail: function (err) {
+          console.log(err)
+        }
+      })
+    }
+
+  }
+})
