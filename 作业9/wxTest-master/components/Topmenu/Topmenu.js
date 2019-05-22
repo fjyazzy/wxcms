8b7b2d474bf509642fe2c664c43d12787c564fe2
Webxinkaifa <1291779571@qq.com>
2019-05-22 15:19:43 +0800

    第一次到第十次作业

---
+++ 作业9/wxTest-master/components/Topmenu/Topmenu.js
@@ -0,0 +1,24 @@
+// components/Topmenu/Topmenu.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    TopMenuList: [{ "mlno": "01", "mlname": "门户", "mltext": "存放门户页面信息" }, { "mlno": "02", "mlname": "文章推荐", "mltext": "特色文章推荐在主界面上显示" }, { "mlno": "03", "mlname": "经典代码", "mltext": "包含WXML,CSS,Javascript, vb.net代码" }, { "mlno": "04", "mlname": "应用案例", "mltext": "各种实际应用系统结构和院里介绍" }, { "mlno": "05", "mlname": "服务购买", "mltext": "提供站点能够提供的服务和报价。" }, { "mlno": "06", "mlname": "环境配置", "mltext": "应用环境配置相关文章" }, { "mlno": "07", "mlname": "首页列表", "mltext": "首页列表管理" }, { "mlno": "08", "mlname": "滚动导航", "mltext": "滚动导航链接" }],
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
