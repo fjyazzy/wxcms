a139b88dd58572bfbd855b06b1617c02ca1369c1
fivefivesix666 <1572956423@qq.com>
2019-05-23 08:46:28 +0800

    cc

---
+++ 作业2/pages/table/table.js
@@ -0,0 +1,17 @@
+Page({
+  data: {
+    listData: [
+      { "code": "01", "text": "text1", "type": "type1" },
+      { "code": "02", "text": "text2", "type": "type2" },
+      { "code": "03", "text": "text3", "type": "type3" },
+      { "code": "04", "text": "text4", "type": "type4" },
+      { "code": "05", "text": "text5", "type": "type5" },
+      { "code": "06", "text": "text6", "type": "type6" },
+      { "code": "07", "text": "text7", "type": "type7" }
+    ]
+  },
+  onLoad: function () {
+    console.log('onLoad')
+  }
+
+})
