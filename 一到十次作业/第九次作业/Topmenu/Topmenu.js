d1bfaaadbb70a3a5b73a7bf080e60a4c8d1f07c1
1539825587 <48699998+1539825587@users.noreply.github.com>
2019-05-23 08:43:17 +0800

    Add files via upload

---
+++ 一到十次作业/第九次作业/Topmenu/Topmenu.js
@@ -0,0 +1,66 @@
+// components/Topmenu/Topmenu.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    TopMenuList: [{ "mlno": "01", "mlname": "门户", "mltext": "存放门户页面信息" }, { "mlno": "02", "mlname": "文章推荐", "mltext": "特色文章推荐" }, { "mlno": "03", "mlname": "经典代码", "mltext": "包含WXML,CSS,JavaScript等代码" }, { "mlno": "04", "mlname": "应用", "mltext": "各种实际应用系统结构和原理介绍" }, { "mlno": "05", "mlname": "购买服务", "mltext": "提供站点能够提供的服务和报价" }, { "mlno": "06", "mlname": "环境配置", "mltext": "应用环境配置相关文章" }, { "mlno": "07", "mlname": "首页列表", "mltext": "首页列表管理" }, { "mlno": "08", "mlname": "滚动导航", "mltext": "滚动导航链接" }, { "mlno": "|", "mlname": "|", "mltext": "|" }]
+    
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
