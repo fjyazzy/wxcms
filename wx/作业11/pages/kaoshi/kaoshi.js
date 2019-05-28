870ee22f49e3490cbff0f3cbd99e1c8dcdb259aa
chencaifa <48703050+chencaifa@users.noreply.github.com>
2019-05-28 18:49:05 +0800

    Add files via upload

---
+++ wx/作业11/pages/kaoshi/kaoshi.js
@@ -0,0 +1,44 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    shuju: {
+
+    },
+    tishi: true,
+    daan: true
+  },
+  onLoad: function () {
+    var timu = {};
+    wx.request({
+      url: 'http://132.232.88.15/cmsv1/apis/getchoiceques.ashx?id=1',
+      success: function (data) {
+        wx.setStorageSync('timu', data)
+      }
+    })
+    timu = wx.getStorageSync('timu')
+    this.setData({
+      shuju: timu
+    })
+  },
+  onTishi: function () {
+    this.setData({
+      tishi: false
+    })
+  },
+  onDaan: function () {
+    this.setData({
+      daan: false
+    })
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
