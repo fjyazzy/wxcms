8224587f5bc431b6a1dc9a034c7b59a5a4496574
lajidongxi <zyq2659245736@163.com>
2019-05-29 20:18:39 +0800

    efef

---
+++ 第十次作业/components/LinkList/LinkList.js
@@ -0,0 +1,36 @@
+// components/LinkList/LinkList.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    ItemList:[
+      {
+        "pagename":"WXML学习的基本语法",
+        "pagetext":"../index/index"
+      },
+      {
+        "pagename": "利息计算机",
+        "pagetext": "../lxjsq/lxjsq"
+      },
+      {
+        "pagename": "地图",
+        "pagetext": "../map/map"
+      }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
