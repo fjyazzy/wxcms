bb207eb9fe1aacc0a682cf87e9a9bd6dbe14cc1b
hgb666 <616100724@qq.com>
2019-05-31 18:33:27 +0800

    第一次到第十一次作业

---
+++ 1-11作业/第9次作业/components/Topmove/Topmove.js
@@ -0,0 +1,67 @@
+// components/Topmove/Topmove.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    TopMove:[{
+
+      name:"aa"
+    },
+      {
+        name: "bb"
+      },
+      {
+        name: "cc"
+      },
+      {
+        name: "dd"
+      },
+      {
+        name: "ee"
+      },
+      {
+        name: "ff"
+      },
+      {
+        name: "gg"
+      },
+      {
+
+        name: "aa"
+      },
+      {
+        name: "bb"
+      },
+      {
+        name: "cc"
+      },
+      {
+        name: "dd"
+      },
+      {
+        name: "ee"
+      },
+      {
+        name: "ff"
+      },
+      {
+        name: "gg"
+      },
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
