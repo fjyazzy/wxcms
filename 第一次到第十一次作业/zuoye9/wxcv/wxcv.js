caeefc1fd573020a2c9317b6bcf17135081aa0cc
1123798379 <1123798379@qq.com>
2019-06-02 13:48:52 +0800

    erferf

---
+++ 第一次到第十一次作业/zuoye9/wxcv/wxcv.js
@@ -0,0 +1,33 @@
+// wxcv/wxcv.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgList: [{
+      imagePath: "/images/1.png",
+      
+    }, {
+      imagePath: "/images/2.png",
+      
+    }, {
+      imagePath: "/images/3.png",
+      
+    }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
