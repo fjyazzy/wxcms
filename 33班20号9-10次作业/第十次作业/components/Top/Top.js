079ec1414f3c008dbde12c275e3eb5df615def5d
lhj1212 <48705646+lhj1212@users.noreply.github.com>
2019-06-03 20:21:04 +0800

    Add files via upload

---
+++ 33班20号9-10次作业/第十次作业/components/Top/Top.js
@@ -0,0 +1,45 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    TopMenuList: [{
+     
+      "mlname": "1",
+      "mltext": "2"
+    }, {
+     
+      "mlname": "3",
+      "mltext": "4"
+    }, {
+      
+      "mlname": "5",
+      "mltext": "6"
+    }, {
+      
+      "mlname": "7",
+      "mltext": "8"
+    }, {
+     
+      "mlname": "9",
+      "mltext": "10"
+    }, {
+      "mlname": "11",
+      "mltext": "12"
+    }]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ 33班20号9-10次作业/第十次作业/components/pmdggl/pmdggl.js
@@ -0,0 +1,35 @@
+// components/pmdggl/pmdggl.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgList: [{
+      imagePath: "/images/one.jpg",
+      pagePath: "../lxjsq/lxjsq"
+    }, {
+      imagePath: "/images/two.jpg",
+      pagePath: "../lyb/lyb"
+    }, {
+      imagePath: "/images/three.jpg",
+      pagePath: "../map/map"
+    }, {
+      imagePath: "/images/four.jpg",
+      pagePath: "../table/table"
+    }]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
