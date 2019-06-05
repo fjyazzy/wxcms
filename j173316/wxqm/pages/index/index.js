c9aaca6afd26cf6fa4c5e91d8e0c3423d05e1d3d
songshumeng <48705570+songshumeng@users.noreply.github.com>
2019-06-05 10:12:54 +0800

    Add files via upload

---
+++ j173316/wxqm/pages/index/index.js
@@ -0,0 +1,71 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  onReady() {
+    this.videoCtx = wx.createVideoContext('myVideo')
+  },
+  play() {
+    this.videoCtx.play()
+  },
+  pause() {
+    this.videoCtx.pause()
+  },
+  /**
+   * 页面的初始数据
+   */
+data: {
+    lists: [],              // 接收搜索的内容
+    wxSearchData: '',       // 输入的值
+  },
+  /**
+     * 搜索
+     */
+  wxSearchInput: function (value) {
+    var that = this;
+    if (value.detail.value.length > 0) {
+      wx.request({
+        url: '',
+        data: {
+          value: value.detail.value
+        },
+        header: { 'content-type': 'application/x-www-form-urlencoded' },
+        method: 'POST',
+        dataType: json,
+        responseType: text,
+        success: function (res) {
+          if (res.code) {
+            var data = that.data.lists;
+            for (let i = 0; i < res.data.length; i++) {
+              data.push(res.data[i]);
+            }
+            that.setData({
+              wxSearchData: value.detail.value,
+              lists: data
+            })
+          }
+        },
+        fail: function (res) { },
+        complete: function (res) { },
+      })
+    }
+  },
+
+  /**
+   * 监听软键盘确认键
+   */
+  wxSearchConfirm: function (e) {
+
+  },
+
+  /**
+   * 返回
+   */
+  back: function (e) {
+    wx: wx.navigateBack({
+      delta: 1,
+    })
+  }
+
+})
