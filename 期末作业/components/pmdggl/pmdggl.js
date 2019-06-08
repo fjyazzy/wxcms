7e95341a4e583f29a3049a0875dbd7737d2fc0af
unknown <tsq47>
2019-06-08 18:59:17 +0800

    期末作业

---
+++ 期末作业/components/pmdggl/pmdggl.js
@@ -0,0 +1,33 @@
+// components/pmgddl/pmgddl.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgList: [{
+        imagePath: "../../images/11.jpg",
+    
+    }, {
+        imagePath:"../../images/22.jpg",
+     
+    }, {
+        imagePath: "../../images/33.jpg",
+  
+    }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
