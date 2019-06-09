f9dbd60dd038d08aaa8ed74d509237cdd0d727a7
klj123456 <15260722871@163.com>
2019-06-09 22:04:41 +0800

    yueke

---
+++ 专项作业/components/Topmove/Topmove.js
@@ -0,0 +1,67 @@
+// components/Topmove/Topmove.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    TopMove:[{
+
+      name:"门达"
+    },
+      {
+        name: "反萨反对方的身份撒"
+      },
+      {
+        name: "打书高手打得过"
+      },
+      {
+        name: "发顺丰"
+      },
+      {
+        name: "豆逼"
+      },
+      {
+        name: "low瓜"
+      },
+      {
+        name: "沙雕"
+      },
+      {
+
+        name: "门达"
+      },
+      {
+        name: "反萨反对方的身份撒"
+      },
+      {
+        name: "打书高手打得过"
+      },
+      {
+        name: "发顺丰"
+      },
+      {
+        name: "豆逼"
+      },
+      {
+        name: "low瓜"
+      },
+      {
+        name: "沙雕"
+      },
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
