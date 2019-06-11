7e4739ffc31bf9b954e11387f9388409ca972fbc
Webxinkaifa <1291779571@qq.com>
2019-06-11 12:12:40 +0800

    专项技能题

---
+++ 专项技能题/wxTest-master/components/Topmenu/Topmenu.js
@@ -0,0 +1,26 @@
+// components/Topmenu/Topmenu.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+TopMenuList: [{ "mlno": "01", "mlname": "wxml系统学习", "mltext": "存放门户页面信息" },
+     { "mlno": "02", "mlname": "利息计算器", "mltext": "特色文章推荐在主界面上显示" },
+      { "mlno": "03", "mlname": "问卷调查", "mltext": "包含WXML,CSS,Javascript, vb.net代码" }, { "mlno": "04", "mlname": "猜字游戏", "mltext": "各种实际应用系统结构和院里介绍" }, { "mlno": "05", "mlname": "用户地图", "mltext": "提供站点能够提供的服务和报价。" }, { "mlno": "06", "mlname": "扫码", "mltext": "应用环境配置相关文章" }, { "mlno": "07", "mlname": "首页列表", "mltext": "首页列表管理" }, { "mlno": "08", "mlname": "滚动导航", "mltext": "滚动导航链接" }],
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
