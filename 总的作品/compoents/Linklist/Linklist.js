d413c60401b6071ccbb0a2bb501bed0c008b5b66
wan1214j <48703538+wan1214j@users.noreply.github.com>
2019-06-11 13:39:48 +0800

    Add files via upload

---
+++ 总的作品/compoents/Linklist/Linklist.js
@@ -0,0 +1,28 @@
+// components/Linklist/Linklist.js
+Component({
+  properties: {
+    usite: {
+      type: String,
+      value: 'http://',
+    },
+  },
+
+  data: {
+    ItemList: [
+      { "pagename": "利息计算器", "pagetext": "../lxjsq/lxjsq" },
+      { "pagename": "地图", "pagetext": "../map/map" },
+      { "pagename": "游戏", "pagetext": "../guess/guess" },
+      { "pagename": "成绩", "pagetext": "../myscore/myscore" },
+      { "pagename": "题库", "pagetext": "../myprofile/myprofile" },
+      { "pagename": "扫码功能", "pagetext": "../sm/sm" },
+      { "pagename": "问卷调查", "pagetext": "../wjdc/wjdc" },
+
+    ],
+  },
+  attached() {
+  },
+  methods: {
+
+  }
+})
+
