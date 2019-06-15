99db5f528ecc5034d7bf2e83c461e75587ab05b1
wbj925 <48507932+wbj925@users.noreply.github.com>
2019-06-15 14:44:13 +0800

    Add files via upload

---
+++ 专项技能作业/components/Topmenu/Topmenu.js
@@ -0,0 +1,26 @@
+// components/Topmenu/Topmenu.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    TopMenuList: [{ "mlno": "01", "mlname": "利息计算器", "mltext": "存放门户页面信息" }, { "mlno": "02", "mlname": "留言板", "mltext": "特色文章准荐在主界面上显示" }, { "mlno": "03", "mlname": "地图", "mltext": "包含WXML,CSS, javascript, vb.net等代码" }, { "mlno": "04", "mlname": "数字游戏", "mltext": "各种实际应用系统结构和原理介绍" }, { "mlno": "05", "mlname": "题库", "mltext": "提供站点能够提供的服务和报价。" }, { "mlno": "06", "mlname": "扫码", "mltext": " 用环境配置相关文章" }, { "mlno": "07", "mlname": "新年快乐", "mltext": "首页列表管理" }, { "mlno": "08", "mlname": "恭喜发财", "mltext": "滚动导航链接" }, {
+      "mlno": "|", "mlname": "大吉大利", "mltext": "|"
+    }],    
+  },
+attached(){
+},
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
