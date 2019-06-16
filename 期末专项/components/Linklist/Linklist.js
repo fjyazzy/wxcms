dcda864ed30c959a3a8cc7d125dc32d411140fce
unknown <zhuangxc>
2019-06-16 22:56:46 +0800

    期末专项

---
+++ 期末专项/components/Linklist/Linklist.js
@@ -0,0 +1,26 @@
+// Linklist/Linklist.js
+Component({
+  data: {
+    ItemList: [ { 
+       pagePath: "/pages/lxjsq/lxjsq",
+      text: "利息计算器"
+      },
+      {
+        pagePath: "/pages/map/map",
+        text: "定位"
+      },
+      {
+        pagePath: "/pages/wjdc/wjdc",
+        text: "问卷调查"
+      },
+      {
+        pagePath: "/pages/hq/hq",
+        text: "获取"
+      }
+    ],
+  },
+  attached() {
+  },
+  methods: {
+  }
+})
---
+++ 期末专项/components/Swipelist/swipelist.js
@@ -0,0 +1,24 @@
+Component({
+  /**   * 组件的初始数据   */
+  data: {
+    newList: [{
+        title: "最新1:",
+        text: "水果大礼包三人团购70元",
+        pagePath:"/pages/guess/guess"
+      },{
+        title: "最新2:",
+        text: "澳洲进口牛肉四人团购70元",
+        pagePath: "/pages/lxjsq/lxjsq"
+      },{
+        title: "最新3:",
+        text: "青春毕业季精美花束95元",
+        pagePath: "/pages/lxjsq/lxjsq" 
+      }
+    ]
+    
+  },
+  attached() {
+  },
+  methods: {
+  }
+})
