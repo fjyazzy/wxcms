a0a36f4123838dfbbee3637160fb8e64e0aaa1c6
yyy1999 <48700115+yyy1999@users.noreply.github.com>
2019-06-17 21:18:07 +0800

    专项技能

---
+++ 专项技能/components/Linklist/Linklist.js
@@ -0,0 +1,34 @@
+// Linklist/Linklist.js
+Component({
+  data: {
+    ItemList: [ { 
+       pagePath: "/pages/lxjsq/lxjsq",
+      text: "利息计算器"
+      },
+      {
+        pagePath: "/pages/guess/guess",
+        text: "猜数游戏"
+      },
+       {
+        pagePath: "/pages/wxapi/wxapi",
+         text: "微信API"
+      },
+      {
+        pagePath: "/pages/wjdc/wjdc",
+        text: "问卷调查"
+      },
+      {
+        pagePath: "/pages/xinxi/xinxi",
+        text: "后端信息"
+      },
+      {
+        pagePath: "/pages/xs/xs",
+        text: "小说书架"
+      }
+    ],
+  },
+  attached() {
+  },
+  methods: {
+  }
+})
---
+++ 专项技能/pages/wjdc/wjdc.js
@@ -0,0 +1,199 @@
+// pages/wjdc/wjdc.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+    currentFatherIndex: 0,
+    questionnaireArray: [
+      {
+        "type": "SCQ",
+        "content": {
+          "description": "你喜欢看电影吗?",
+          "options":
+            [
+              { "id": 1, "name": "每周都看", "isSelected": false },
+              { "id": 2, "name": "偶尔看", "isSelected": false },
+              { "id": 3, "name": "几乎不看", "isSelected": false }
+            ]
+        }
+      },
+      {
+        "type": "MCQ",
+        "content": {
+          "description": "你喜欢看书吗?",
+          "options":
+            [
+              { "id": 1, "name": "喜欢", "isSelected": false },
+              { "id": 2, "name": "一般", "isSelected": false },
+              { "id": 3, "name": "不喜欢", "isSelected": false }
+            ]
+        }
+      },
+      {
+        "type": "MCQ",
+        "content": {
+          "description": "你经常玩游戏吗?",
+          "options":
+            [
+              { "id": 1, "name": "经常", "isSelected": false },
+              { "id": 2, "name": "偶尔", "isSelected": false },
+              { "id": 3, "name": "一般不玩", "isSelected": false }
+            ]
+        }
+      },
+      {
+        "type": "SAQ",
+        "content": {
+          "description": "你最喜欢的食物口味是?",
+          "options":
+            [
+              { "id": 1, "name": "甜", "isSelected": false },
+              { "id": 2, "name": "咸", "isSelected": false },
+              { "id": 3, "name": "辣", "isSelected": false },
+              { "id": 4, "name": "其他", "isSelected": false }
+            ]
+        }
+      },
+      {
+        "type": "SAQ",
+        "content": {
+          "description": "你经常运动吗?",
+          "options":
+            [
+              { "id": 1, "name": "经常", "isSelected": false },
+              { "id": 2, "name": "偶尔", "isSelected": false },
+              { "id": 3, "name": "几乎不去", "isSelected": false }
+            ]
+        }
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options.id)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  goBack: function () {
+    console.log('to task page')
+    wx.switchTab({
+      url: '../task/task',
+    })
+  },
+
+  getTempFatherIndex: function (input) {
+    var tempFatherIndex = input.currentTarget.dataset.id;
+    //console.log('currentFatherIndex: ' + tempFatherIndex);
+    this.setData({
+      currentFatherIndex: tempFatherIndex,
+    });
+  },
+
+  radioChangeSCQ: function (input) {
+    var tempFatherIndex = this.data.currentFatherIndex;
+    var tempArray = this.data.questionnaireArray;
+    for (var i in tempArray[tempFatherIndex].content.options) {
+      if (tempArray[tempFatherIndex].content.options[i].name == input.detail.value) {
+        tempArray[tempFatherIndex].content.options[i].isSelected = true;
+      }
+      else {
+        tempArray[tempFatherIndex].content.options[i].isSelected = false;
+      }
+    }
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  checkboxChangeMCQ: function (input) {
+    // console.log(input.detail.value);
+    var flag = false;
+    var tempFatherIndex = this.data.currentFatherIndex;
+    var tempArray = this.data.questionnaireArray;
+    for (var i in tempArray[tempFatherIndex].content.options) {
+      flag = false;
+      for (var j in input.detail.value) {
+        if (tempArray[tempFatherIndex].content.options[i].name == input.detail.value[j]) {
+          flag = true;
+        }
+      }
+      if (flag == true) {
+        tempArray[tempFatherIndex].content.options[i].isSelected = true;
+      }
+      else {
+        tempArray[tempFatherIndex].content.options[i].isSelected = false;
+      }
+    }
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  bindblurAnswerOfSAQ: function (input) {
+    var tempIndex = input.currentTarget.dataset.id;
+    var tempArray = this.data.questionnaireArray;
+    tempArray[tempIndex].content.answer = input.detail.value;
+    // console.log(tempArray[tempIndex].content);
+    this.setData({
+      questionnaireArray: tempArray,
+    });
+  },
+
+  complete: function () {
+    console.log(this.data.questionnaireArray);
+  },
+})
