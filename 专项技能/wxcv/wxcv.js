2c3446e839667ed4dc1a09d4fcd1b63c3f699a46
wenggezuishuai <2429968796@qq.com>
2019-06-21 21:27:22 +0800

    efg

---
+++ 专项技能/wxcv/wxcv.js
@@ -0,0 +1,33 @@
+// wxcv/wxcv.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgList: [{
+      imagePath: "/images/1.jpg",
+      
+    }, {
+      imagePath: "/images/2.jpg",
+      
+    }, {
+      imagePath: "/images/3.jpg",
+      
+    }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
