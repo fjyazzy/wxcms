efa2eae0110a2746bef6bc86e95810cc70aa069f
吴端淞 <48464490+wds937@users.noreply.github.com>
2019-06-22 15:45:43 +0800

    Add files via upload

---
+++ zhuanxiang/components/banner/banner.js
@@ -0,0 +1,33 @@
+// components/pmdggl/pmdggl.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgList: [{
+      imagePath: "/image/banner/ (1).jpg",
+      pagePath: "/pages/calc/calc"
+    }, {
+        imagePath: "/image/banner/ (3).jpg",
+      pagePath: "/pages/lxjsq/lxjsq"
+    }, {
+        imagePath: "/image/banner/ (2).jpg",
+      pagePath: "/pages/lxjsq/lxjsq"
+    }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
